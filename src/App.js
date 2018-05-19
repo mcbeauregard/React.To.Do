6cc36b06fa0d3ae159ad346e04454ca17e61bcab
mcbeauregard <mcbeauregard@gmail.com>
2018-05-18 22:31:34 -0700

    Checkpoint React To-Do: Events Complete

--- src/App.js
+++ src/App.js
@@ -33,17 +33,24 @@ class App extends Component {
     this.setState({ todos: todos });
   }
 
+  deleteTodo(index) { // grabs the item we want to delete. Data is stored here and then rendered, and passed to ToDo.js
+    var newItems = this.state.todos.filter((todo) => { // discards item we want to delete, and displays the rest of the items.
+    return todo != index}); // returns input item, not specified to be deleted.
+    this.setState({ todos: newItems }); // passes the new state of itme discarded, returning remaing items.
+  }
+
   render() {
     return (
       <div className="App">
       <ul>
         { this.state.todos.map( (todo, index) =>
-          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+          <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(todo)} /> // tell us which todo is to be deleted allowing us to pass it ToDo.js
         )}
       </ul>
       <form onSubmit={ (e) => this.handleSubmit(e) }>
         <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) } />
         <input type="submit" />
+
       </form>
       </div>
     );
